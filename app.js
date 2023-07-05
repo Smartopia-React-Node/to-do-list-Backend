0d1f33be86c31f863109f570dcfb4ce8c79d0431
Rohit Mukherjee <114508274+RohitMukherjee47@users.noreply.github.com>
2023-07-05 13:57:53 +0530

    Update app.js

--- app.js
+++ app.js
@@ -10,6 +10,9 @@ const app = express();
 
 app.use(bodyParser.urlencoded({extended: true}));
 
+app.get("/", (req, res) => {
+    res.send("<h1>API CONNECTED</h1>");
+})
 app.get("/api/list", async(req, res) => {
     try {
         const myData = await List.find();
@@ -19,23 +22,24 @@ app.get("/api/list", async(req, res) => {
     }
 });
 app.post("/api/list", async(req, res) => {
+    const newData = new List(req.query);
     try {
-        const newData = (req.query);
-        console.log(req.query);
-        List.create(newData);
-        res.status(200).send("data stored");
+        const data = await newData.save(); //returns object|json 
+        console.log(data);
+        res.status(200).json(data);
     } catch (error) {
+        res.send(error);
         console.log(error);
     }
 });
 app.delete("/api/list/del/:id", async(req, res) => {
     try {
-        const item = await List.findByIdAndDelete(req.params.id);
-        res.status(200).send("data deleted");
+        const data = await List.findByIdAndDelete(req.params.id);
+        res.status(200).json(data);
     } catch (error) {
         console.log(error);
     }
-})
+});
 
 const start = async () => {
     try {
@@ -48,3 +52,29 @@ const start = async () => {
     }
 };
 start();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/* OLD VERSION FOR INSERT
+    try {
+        const newData = (req.query);
+        console.log(req.query);
+        await List.create(newData);
+        res.status(200).send("data stored");
+    } catch (error) {
+        console.log(error);
+    }
+*/
